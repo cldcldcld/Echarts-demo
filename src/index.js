e01ceefbe82047e3ef4015d6d438025b1951a136
mark.zhu <mark.zhu@thermofisher.com>
2018-05-17 17:23:09 +0800

    add radar image

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Route, BrowserRouter } from 'react-router-dom'
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render((
   <BrowserRouter>
-  	<div>
+  	<div style="height: 100%">
      	<Route path="/" exact component={App}/>
      	<Route path="/baseRadarChart" component={baseRadarChart}/>
     </div>
