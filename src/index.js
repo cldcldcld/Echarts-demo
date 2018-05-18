1c0d9c26aeab94f5ff35d8fd16e2ed3886a60e5c
mark.zhu <mark.zhu@thermofisher.com>
2018-05-18 16:49:48 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import baseRadarChart from './radar/baseRadarChart';
+import radarChart from './radarImage/radarChart';
 import registerServiceWorker from './registerServiceWorker';
 import { Route, BrowserRouter } from 'react-router-dom'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render((
   <BrowserRouter>
-  	<div style="height: 100%">
+  	<div style={{height: '100%'}}>
      	<Route path="/" exact component={App}/>
      	<Route path="/baseRadarChart" component={baseRadarChart}/>
+     	<Route path="/radarChart" component={radarChart}/>
     </div>
   </BrowserRouter>
 ), document.getElementById('root'))
---
+++ src/radarImage/radarChart.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import echarts from 'echarts';
+import './radarChart.css'
+
+class radarChart extends Component {
+  render() {
+    return (
+      <div className='radarChart'>
+        <div id='radarChart'></div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+
+    var myChart = echarts.init(document.getElementById('radarChart'));
+
+    myChart.setOption({
+      angleAxis: {
+          type: 'category',
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+          z: 10
+      },
+      radiusAxis: {
+      },
+      polar: {
+      },
+      series: [{
+          type: 'bar',
+          data: [1, 2, 3, 4, 3, 5, 1],
+          coordinateSystem: 'polar',
+          name: 'A',
+          stack: 'a'
+      }, {
+          type: 'bar',
+          data: [2, 4, 6, 1, 3, 2, 1],
+          coordinateSystem: 'polar',
+          name: 'B',
+          stack: 'a'
+      }, {
+          type: 'bar',
+          data: [1, 2, 3, 4, 1, 2, 5],
+          coordinateSystem: 'polar',
+          name: 'C',
+          stack: 'a'
+      }],
+      legend: {
+          show: true,
+          data: ['A', 'B', 'C']
+      }      
+    });
+
+  }
+}
+
+export default radarChart;
