334b4ff5846fdb203111f783b0eac5e76a558e22
mark.zhu <mark.zhu@thermofisher.com>
2018-05-29 14:41:48 +0800

    commit at home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import echarts from 'echarts';
 import bmap from 'echarts/extension/bmap/bmap.js';
+import {MP} from './map.js';
 
 class App extends Component {
   render() {
@@ -15,83 +16,88 @@ class App extends Component {
   }
 
   componentDidMount() {
-
-    var BMap = window.BMap
-
     var data = require('./data.json')
-    var myChart = echarts.init(document.getElementById('main'));
 
-    // axios.get('data/asset/data/hangzhou-tracks.json').then(function (data) {
+    MP('e6yiGBqNtg6jlVwRXEa54zLiMfI9xBZk').then(BMap => {
 
-    //     var points = [].concat.apply([], data.data.map(function (track) {
-    //         return track.map(function (seg) {
-    //             return seg.coord.concat([1]);
-    //         });
-    //     }));
-    //     myChart.setOption({
-    //         animation: false,
-    //         bmap: {
-    //             center: [120.13066322374, 30.240018034923],
-    //             zoom: 14,
-    //             roam: true
-    //         },
-    //         visualMap: {
-    //             show: false,
-    //             top: 'top',
-    //             min: 0,
-    //             max: 5,
-    //             seriesIndex: 0,
-    //             calculable: true,
-    //             inRange: {
-    //                 color: ['blue', 'blue', 'green', 'yellow', 'red']
-    //             }
-    //         },
-    //         series: [{
-    //             type: 'heatmap',
-    //             coordinateSystem: 'bmap',
-    //             data: points,
-    //             pointSize: 5,
-    //             blurSize: 6
-    //         }]
-    //     });
 
-    // });
 
-    var points = [].concat.apply([], data.map(function (track) {
-        return track.map(function (seg) {
-            return seg.coord.concat([1]);
+        var myChart = echarts.init(document.getElementById('main'));
+
+        // axios.get('data/asset/data/hangzhou-tracks.json').then(function (data) {
+
+        //     var points = [].concat.apply([], data.data.map(function (track) {
+        //         return track.map(function (seg) {
+        //             return seg.coord.concat([1]);
+        //         });
+        //     }));
+        //     myChart.setOption({
+        //         animation: false,
+        //         bmap: {
+        //             center: [120.13066322374, 30.240018034923],
+        //             zoom: 14,
+        //             roam: true
+        //         },
+        //         visualMap: {
+        //             show: false,
+        //             top: 'top',
+        //             min: 0,
+        //             max: 5,
+        //             seriesIndex: 0,
+        //             calculable: true,
+        //             inRange: {
+        //                 color: ['blue', 'blue', 'green', 'yellow', 'red']
+        //             }
+        //         },
+        //         series: [{
+        //             type: 'heatmap',
+        //             coordinateSystem: 'bmap',
+        //             data: points,
+        //             pointSize: 5,
+        //             blurSize: 6
+        //         }]
+        //     });
+
+        // });
+
+        var points = [].concat.apply([], data.map(function (track) {
+            return track.map(function (seg) {
+                return seg.coord.concat([1]);
+            });
+        }));
+
+        myChart.setOption({
+            animation: false,
+            bmap: {
+                center: [120.13066322374, 30.240018034923],
+                zoom: 14,
+                roam: true
+            },
+            visualMap: {
+                show: false,
+                top: 'top',
+                min: 0,
+                max: 5,
+                seriesIndex: 0,
+                calculable: true,
+                inRange: {
+                    color: ['blue', 'blue', 'green', 'yellow', 'red']
+                }
+            },
+            series: [{
+                type: 'heatmap',
+                coordinateSystem: 'bmap',
+                data: points,
+                pointSize: 5,
+                blurSize: 6
+            }]
         });
-    }));
 
-    myChart.setOption({
-        animation: false,
-        bmap: {
-            center: [120.13066322374, 30.240018034923],
-            zoom: 14,
-            roam: true
-        },
-        visualMap: {
-            show: false,
-            top: 'top',
-            min: 0,
-            max: 5,
-            seriesIndex: 0,
-            calculable: true,
-            inRange: {
-                color: ['blue', 'blue', 'green', 'yellow', 'red']
-            }
-        },
-        series: [{
-            type: 'heatmap',
-            coordinateSystem: 'bmap',
-            data: points,
-            pointSize: 5,
-            blurSize: 6
-        }]
+        // var bmap = chart.getModel().getComponent('bmap').getBMap();
+        // bmap.addControl(new BMap.MapTypeControl());
+
+
     });
-
-    // var bmap = chart.getModel().getComponent('bmap').getBMap();
-    // bmap.addControl(new BMap.MapTypeControl());
 
 
   }
